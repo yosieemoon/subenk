91c0d80a4e317303e8fb020e6362af17ef8f210a
yosieemoon <yosapat4@gmail.com>
2026-07-02 23:55:58 +0700

    Update w-trade.lua

--- w-trade.lua
+++ w-trade.lua
@@ -65,7 +65,7 @@ _G.GAGConfig = _G.GAGConfig or {
     },
     ["Mail"] = {
         ["Auto Claim"] = true,
-        ["Send To"]    = "kkndesapenari33",
+        ["Send To"]    = "soooyoooo0",
         ["Send"]       = {
             "Moon Bloom", "Dragon's Breath", "Gold", "Rainbow", "Venom Spitter", "Mega", "Hypno Bloom",{ Item = "Carrot", Count = 20 },
             "Raccoon",
